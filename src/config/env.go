9ee8aa033c1c2366dc82a092f75543e29364e8fd
quantumvividly <229402092+quantumvividly@users.noreply.github.com>
2025-12-11 13:46:33 +0100

    Debug (#2)

--- src/config/env.go
+++ src/config/env.go
@@ -18,6 +18,24 @@ func getEnv(variable string, fallback ...string) string {
 	return value
 }
 
+func getEnvBool(variable string, fallback ...bool) bool {
+	value := os.Getenv(variable)
+	if value == "" {
+		if len(fallback) == 0 {
+			log.Fatalf("Env var %s must be set.", variable)
+		} else {
+			return fallback[0]
+		}
+	}
+
+	valueI, err := strconv.ParseBool(value)
+	if err != nil {
+		log.Fatalf("Failed to convert string to bool for %s", variable)
+	}
+
+	return valueI
+}
+
 func getEnvInt(variable string, fallback ...int) int {
 	value := os.Getenv(variable)
 	if value == "" {
