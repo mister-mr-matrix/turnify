15522a4fe3e2490dd5c2f7b69e0449e90c20806a
quantumvividly <229402092+quantumvividly@users.noreply.github.com>
2025-12-11 13:46:02 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"log/slog"
+	"os"
 	_ "time/tzdata"
 
 	"github.com/mister-mr-matrix/turnify/src/config"
@@ -10,8 +12,17 @@ import (
 
 func main() {
 	cfg := config.New()
-	rtr := router.New(cfg)
 
+	if cfg.Debug {
+		slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		})))
+	} else {
+		slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+	}
+	slog.Debug("DEBUG logging enabled!")
+
+	rtr := router.New(cfg)
 	ctx := context.Background()
 	rtr.Start(ctx)
 }
